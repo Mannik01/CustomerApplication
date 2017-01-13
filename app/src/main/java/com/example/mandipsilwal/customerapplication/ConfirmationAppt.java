8c932a4e09eae7e8cc89daeceeaab4a0afdbf10c
sosal4 <sosal4@hotmail.com>
2017-01-13 11:54:22 -0600

    changes on the login and my appointment

--- app/src/main/java/com/example/mandipsilwal/customerapplication/ConfirmationAppt.java
+++ app/src/main/java/com/example/mandipsilwal/customerapplication/ConfirmationAppt.java
@@ -1,16 +1,13 @@
 package com.example.mandipsilwal.customerapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ConfirmationAppt extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_confirmation_appt);
     }
-
 }
--- app/src/main/java/com/example/mandipsilwal/customerapplication/MyAppointment.java
+++ app/src/main/java/com/example/mandipsilwal/customerapplication/MyAppointment.java
@@ -5,12 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MyAppointment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_my_appointment);
+
     }
 
 }
