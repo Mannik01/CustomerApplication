83b117395f1786758a5c7df4fd305d975a508e98
therealshrey <TheRealShrey@Shreys-MacBook-Pro.local>
2017-01-13 12:07:55 -0600

    search bar created

---
+++ app/src/main/java/com/example/mandipsilwal/customerapplication/search.java
@@ -0,0 +1,19 @@
+package com.example.mandipsilwal.customerapplication;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class search extends drawer {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //inflate your activity layout here!
+        View contentView = inflater.inflate(R.layout.activity_search, null, false);
+        drawer.addView(contentView, 0);
+    }
+}
